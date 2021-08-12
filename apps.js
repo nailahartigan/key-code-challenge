f632d815680df0d56c38857b150b945401751e72
nailahartigan <nailahartigan@gmail.com>
2021-08-12 11:40:52 +0100

    modified all files to key-code

--- apps.js
+++ apps.js
@@ -4,15 +4,15 @@
 
 const key = document.getElementById("key")
 const header = document.getElementById("header")
-const eventkey = document.getElementById("eventkey")
-const eventcode = document.getElementById("eventcode")
-const eventwhich = document.getElementById("eventwhich")
+const eventkey = document.getElementById("eventKey")
+const eventcode = document.getElementById("eventCode")
+const eventwhich = document.getElementById("eventWhich")
 
 // listening for a key down event
 
 document.addEventListener("keydown", (e) => {
     header.textContent = e.keyCode
-    eventcode.textContent = e
-    
-    console.log(e)
+    eventkey.textContent =event.key
+    eventcode.textContent = event.code
+    eventwhich.textContent = event.which
 })
